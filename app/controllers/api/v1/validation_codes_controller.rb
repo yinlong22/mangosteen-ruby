7f6ec27cbf203491fa3ca19711dd22aa35c7b355
shiyinlong <342241857@qq.com>
2022-07-05 12:04:52 +0800

    feat: 添加validationCodes controller的测试

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
-    head 202
+    code = SecureRandom.random_number.to_s[2..7]
+    validation_codes = ValidationCode.new email: params[:email], kind: 'sign_in', code: code
+    if validation_codes.save
+      head 200
+    else
+      render json: { error: validation_codes.errors }
+    end
   end
 end
