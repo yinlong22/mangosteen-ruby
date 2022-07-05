e3ed951a26a2959e6cc1b89ff8aec89e2d495c94
shiyinlong <342241857@qq.com>
2022-07-05 11:34:34 +0800

    feat: 添加items controller的测试

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   def show
-    items = Item.page(params[:page]).per(10)
+    items = Item.page(params[:page])
     render json: {
       resources: items,
       pager: {
@@ -12,7 +12,7 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new amount: 1
+    item = Item.new amount: params[:amount]
     if item.save
       render json: { resource: item }
     else
